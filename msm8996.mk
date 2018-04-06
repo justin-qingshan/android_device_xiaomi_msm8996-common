b4bfb6cf7d9206d1cdab00fa89a098b1943f6bc0
Weijia WANG <9713184+wegank@users.noreply.github.com>
2018-04-06 11:03:10 +0800

    msm8996-common: Fix lineage power deadlock

--- msm8996.mk
+++ msm8996.mk
@@ -301,8 +301,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti \
-    vendor.lineage.power@1.0 \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
     vendor.lineage.power@1.0_vendor
 
 PRODUCT_COPY_FILES += \
